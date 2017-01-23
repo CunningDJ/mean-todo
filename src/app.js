314f4d92d7af14d35867acd87a1b406ffe17c755
CunningDJ <david7789@gmail.com>
2017-01-22 22:15:11 -0600

    Adding first API elements.

--- src/app.js
+++ src/app.js
@@ -1,14 +1,31 @@
 'use strict';
 
+// variables
 var express = require('express');
-
 var app = express();
+var router = express.Router();
 
 var PORT = 3000;
 
-app.listen(3000, listenFunc);
+// namespace
+app.use('/', express.static('public'));
+app.use('/api', router)
+
+// routes
+router.get('/todos', todoRoute);
 
 
-function listenFunc() {
+
+
+app.listen(3000, listener);
+
+
+// functions
+function listener() {
     console.log("The server is running on port 3000!");
+}
+
+function todoRoute(req,res) {
+    var testJSON = {todos:[]};
+    res.send(testJSON);
 }
